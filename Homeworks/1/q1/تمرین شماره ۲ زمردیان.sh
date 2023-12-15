cf3522aa8ecb2a992fdaedad54c4fc43a0aa1497
smzomorodian <150505712+smbzomorodian@users.noreply.github.com>
2023-12-15 11:51:50 +0330

    Update تمرین شماره ۲ زمردیان.sh

--- Homeworks/1/q1/تمرین شماره ۲ زمردیان.sh
+++ Homeworks/1/q1/تمرین شماره ۲ زمردیان.sh
@@ -1,3 +1,1 @@
-pip install -U $(cat <(pip freeze --local | awk -F = '{print $1}'))
------------------------------------------------------------------------------------------------------
-pip3 list --format=json | jq -r '.[] | .name' | while read name; do pip3 install -U "$name"; done
+
